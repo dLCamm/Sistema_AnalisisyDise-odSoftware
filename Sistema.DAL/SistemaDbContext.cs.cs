eaeaf575e7df0128fefff0237aa96bef466e86ad
unknown <punnn005@proton.me>
2026-04-01 13:35:46 -0600

    feat(db): Se creo la entidad 'Producto' y la tabla de 'Productos' de la base de datos

---
+++ Sistema.DAL/SistemaDbContext.cs.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+using Sistema.Entities;
+
+namespace Sistema.DAL
+{
+    public class SistemaDbContext : DbContext
+    {
+        public DbSet<Producto> Productos { get; set; }
+
+        public SistemaDbContext(DbContextOptions<SistemaDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Producto>(entity =>
+            {
+                entity.ToTable("Productos");
+
+                entity.HasKey(p => p.Id);
+
+                entity.Property(p => p.Id)
+                      .ValueGeneratedOnAdd();
+
+                entity.Property(p => p.Nombre)
+                      .IsRequired()
+                      .HasMaxLength(100);
+
+                entity.Property(p => p.Descripcion);
+
+                entity.Property(p => p.PrecioCompra)
+                      .HasColumnType("decimal(10,2)");
+
+                entity.Property(p => p.PrecioVenta)
+                      .HasColumnType("decimal(10,2)");
+
+                entity.Property(p => p.Stock);
+
+                entity.Property(p => p.StockMinimo);
+
+                entity.Property(p => p.FechaCreacion);
+
+                entity.Property(p => p.Estado)
+                      .HasMaxLength(20);
+            });
+        }
+    }
+}
---
+++ Sistema.Entities/producto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sistema.Entities
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public decimal PrecioCompra { get; set; }
+
+        public decimal PrecioVenta { get; set; }
+
+        public int Stock { get; set; }
+
+        public int StockMinimo { get; set; }
+
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+
+        public string Estado { get; set; }
+    }
+}
