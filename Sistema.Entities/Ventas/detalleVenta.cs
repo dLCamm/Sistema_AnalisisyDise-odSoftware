a8740031510f8bd8c90bc63a19e183caf2345e54
unknown <punnn005@proton.me>
2026-04-09 22:15:27 -0600

    feat(ventas): Creacion de entidades para ventas

---
+++ Sistema.Entities/Ventas/detalleVenta.cs
@@ -0,0 +1,21 @@
+﻿using Sistema.Entities.Productos;
+
+
+namespace Sistema.Entities.Ventas
+{
+    public class DetalleVenta
+    {
+        public int Id { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public decimal Subtotal { get; set; }
+
+        // FK
+        public int VentaId { get; set; }
+        public int ProductoId { get; set; }
+
+        // Navegación
+        public Venta Venta { get; set; } = null!;
+        public Producto Producto { get; set; } = null!;
+    }
+}
