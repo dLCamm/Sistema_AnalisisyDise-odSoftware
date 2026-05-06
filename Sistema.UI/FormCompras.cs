cccb43974659d16558186a5603615dfaade28746
dLCamm <camilodeleon229@gmail.com>
2026-05-06 15:04:12 -0600

    design: Formato inicial Grafico de Compras

---
+++ Sistema.UI/FormCompras.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sistema.UI
+{
+    public partial class FormCompras : Form
+    {
+        public FormCompras()
+        {
+            InitializeComponent();
+        }
+    }
+}
