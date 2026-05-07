a43dbae9cee4da7e0aa30d6fee99bb8ae296507a
unknown <punnn005@proton.me>
2026-05-06 18:11:14 -0600

    feat(creditos): se agregaron las entidades para el modulo de creditos

---
+++ Sistema.Entities/Creditos/abono.cs
@@ -0,0 +1,17 @@
+﻿namespace Sistema.Entities.Creditos
+{
+    public class Abono
+    {
+        public int Id { get; set; }
+
+        public decimal Monto { get; set; }
+
+        public DateTime Fecha { get; private set; } = DateTime.Now;
+
+        // FK
+        public int CreditoId { get; set; }
+
+        // Navegación
+        public Credito Credito { get; set; } = null!;
+    }
+}
