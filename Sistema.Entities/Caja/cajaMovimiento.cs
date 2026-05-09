58db29ee963eaafb8ab6c99b62c610a0830aa6e1
unknown <punnn005@proton.me>
2026-05-09 13:41:39 -0600

    feat(caja): se agrego las entidades para el modulo de caja

---
+++ Sistema.Entities/Caja/cajaMovimiento.cs
@@ -0,0 +1,22 @@
+﻿namespace Sistema.Entities.Caja
+{
+    public class MovimientoCaja
+    {
+        public int Id { get; set; }
+
+        public TipoMovimientoCaja Tipo { get; set; }
+
+        public decimal Monto { get; set; }
+
+        public OrigenMovimientoCaja Origen { get; set; }
+
+        public string Descripcion { get; set; } = string.Empty;
+
+        public DateTime Fecha { get; private set; } = DateTime.Now;
+
+        public EstadoMovimientoCaja Estado { get; set; }
+
+        // Usuario que realizo el movimiento
+        public int? UsuarioId { get; set; }
+    }
+}
