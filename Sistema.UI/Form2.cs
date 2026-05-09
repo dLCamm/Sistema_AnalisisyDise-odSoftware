ced7f66cd24f165fb9cd8b461484e4d1392421ff
unknown <punnn005@proton.me>
2026-05-08 20:45:34 -0600

    fix(UI - ventas): se corrigio un error de asignacion ocurrido por un cambio en el modelo de venta

--- Sistema.UI/Form2.cs
+++ Sistema.UI/Form2.cs
@@ -261,7 +261,6 @@ namespace Sistema.UI
             {
                 ClienteId = clienteId,
                 UsuarioId = usuarioId,
-                Fecha = DateTime.Now,
                 TipoPago = tipoPago,
                 Total = CalcularTotal()
             };
