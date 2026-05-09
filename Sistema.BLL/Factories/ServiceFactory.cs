b7090079c3f75146d49f00df457019b7c8f4ef87
unknown <punnn005@proton.me>
2026-05-08 20:43:25 -0600

    feat(creditos, compras, proveedores): se agregaron los servicios de los respectivos modulos

--- Sistema.BLL/Factories/ServiceFactory.cs
+++ Sistema.BLL/Factories/ServiceFactory.cs
@@ -18,7 +18,9 @@ namespace Sistema.BLL.Factories
             var context = new SistemaDbContext(_options);
             var repo = new ProductoRepository(context);
 
-            return new InventarioService(context, repo);
+            return new InventarioService(
+                context, 
+                repo);
         }
         public static VentaService CrearVentaService()
         {
@@ -27,7 +29,11 @@ namespace Sistema.BLL.Factories
             var repoProducto = new ProductoRepository(context);
             var repoClientes = new ClienteRepository(context);
 
-            return new VentaService(context, repoVenta, repoProducto, repoClientes);
+            return new VentaService(
+                context, 
+                repoVenta, 
+                repoProducto, 
+                repoClientes);
         }
 
         public static ClienteService CrearClienteService()
@@ -35,8 +41,47 @@ namespace Sistema.BLL.Factories
             var context = new SistemaDbContext(_options);
             var repo = new ClienteRepository(context);
 
-            return new ClienteService(context, repo);
+            return new ClienteService(
+                context, 
+                repo);
         }
 
+        public static ProveedorService CrearProveedorService()
+        {
+            var context = new SistemaDbContext(_options);
+            var repo = new ProveedorRepository(context);
+
+            return new ProveedorService(
+                context, 
+                repo);
+        }
+
+        public static CompraService CrearCompraService()
+        {
+            var context = new SistemaDbContext(_options);
+
+            var repoCompra = new CompraRepository(context);
+            var repoProducto = new ProductoRepository(context);
+            var repoProveedor = new ProveedorRepository(context);
+
+            return new CompraService(
+                context,
+                repoCompra,
+                repoProducto,
+                repoProveedor);
+        }
+
+        public static CreditoService CrearCreditoService()
+        {
+            var context = new SistemaDbContext(_options);
+
+            var repoCredito = new CreditoRepository(context);
+            var repoVenta = new VentaRepository(context);
+
+            return new CreditoService(
+                context,
+                repoCredito,
+                repoVenta);
+        }
     }
 }
---
+++ Sistema.BLL/Services/ProveedorService.cs
@@ -0,0 +1,121 @@
+﻿using Sistema.DAL.Data;
+using Sistema.DAL.Repositories.Interfaces;
+using Sistema.Entities.Proveedores;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sistema.BLL.Services
+{
+    public class ProveedorService : IDisposable
+    {
+        private readonly SistemaDbContext _context;
+        private readonly IProveedorRepository _repo;
+
+        public ProveedorService(
+            SistemaDbContext context,
+            IProveedorRepository repo)
+        {
+            _context = context;
+            _repo = repo;
+        }
+
+        // REGISTRAR
+        public void RegistrarProveedor(
+            string nombre,
+            string telefono,
+            string direccion,
+            string email)
+        {
+            if (string.IsNullOrWhiteSpace(nombre))
+                throw new Exception("El nombre es obligatorio");
+
+            var proveedor = new Proveedor
+            {
+                Nombre = nombre,
+                Telefono = telefono,
+                Direccion = direccion,
+                Email = email,
+                Estado = EstadoProveedor.Activo
+            };
+
+            _repo.Insertar(proveedor);
+
+            _context.SaveChanges();
+        }
+
+        // ACTUALIZAR
+        public void ActualizarProveedor(
+            int id,
+            string nombre,
+            string telefono,
+            string direccion,
+            string email,
+            EstadoProveedor estado)
+        {
+            var proveedor = _repo.ObtenerPorId(id);
+
+            if (proveedor == null)
+                throw new Exception("Proveedor no encontrado");
+
+            proveedor.Nombre = nombre;
+            proveedor.Telefono = telefono;
+            proveedor.Direccion = direccion;
+            proveedor.Email = email;
+            proveedor.Estado = estado;
+
+            _repo.Actualizar(proveedor);
+
+            _context.SaveChanges();
+        }
+
+        // ANULAR
+        public void AnularProveedor(int id)
+        {
+            var proveedor = _repo.ObtenerPorId(id);
+
+            if (proveedor == null)
+                throw new Exception("Proveedor no encontrado");
+
+            proveedor.Estado = EstadoProveedor.Inactivo;
+
+            _repo.Actualizar(proveedor);
+
+            _context.SaveChanges();
+        }
+
+        // OBTENER POR ID
+        public Proveedor ObtenerProveedor(int id)
+        {
+            var proveedor = _repo.ObtenerPorId(id);
+
+            if (proveedor == null)
+                throw new Exception("Proveedor no encontrado");
+
+            return proveedor;
+        }
+
+        // LISTAR TODOS
+        public List<Proveedor> ListarProveedores()
+        {
+            return _repo.ObtenerTodos();
+        }
+
+        // LISTAR ACTIVOS
+        public List<Proveedor> ListarActivos()
+        {
+            return _repo.ObtenerPorEstado(EstadoProveedor.Activo);
+        }
+
+        // LISTAR INACTIVOS
+        public List<Proveedor> ListarInactivos()
+        {
+            return _repo.ObtenerPorEstado(EstadoProveedor.Inactivo);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
