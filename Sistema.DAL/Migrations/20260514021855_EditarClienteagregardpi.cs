6d48b7317474ffced4a4af17a7da70161f52431d
unknown <punnn005@proton.me>
2026-05-13 21:03:36 -0600

    feat(clientes): se agrego el dip como segundo identificador de un cliente

---
+++ Sistema.DAL/Migrations/20260514021855_EditarClienteagregardpi.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sistema.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditarClienteagregardpi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DPI",
+                table: "Clientes",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DPI",
+                table: "Clientes");
+        }
+    }
+}
