e3737be66c8c20e44fe40eb52b1ce82c9bf2539b
unknown <punnn005@proton.me>
2026-05-14 20:47:20 -0600

    feat(reportes): creacion de DTOs

---
+++ Sistema.DAL/Repositories/Interfaces/IReporteRepository.cs
@@ -0,0 +1,78 @@
+﻿using Sistema.Entities.DTOs.Caja;
+using Sistema.Entities.DTOs.Compras;
+using Sistema.Entities.DTOs.Creditos;
+using Sistema.Entities.DTOs.Dashboard;
+using Sistema.Entities.DTOs.Inventario;
+using Sistema.Entities.DTOs.Ventas;
+
+
+namespace Sistema.DAL.Repositories.Interfaces
+{
+    public interface IReporteRepository
+    {
+        // =========================
+        // COMPRAS
+        // =========================
+        List<CompraReporteDTO>
+            ObtenerComprasPorFecha(
+            DateTime fechaInicio,
+            DateTime fechaFin);
+
+        List<CompraReporteDTO>
+            ObtenerComprasProveedor(
+            int proveedorId);
+
+        decimal ObtenerTotalCompras(
+            DateTime fechaInicio,
+            DateTime fechaFin);
+
+        List<CompraPorDiaDTO>
+            ObtenerComprasPorDia(
+            DateTime fechaInicio,
+            DateTime fechaFin);
+
+        // =========================
+        // VENTAS
+        // =========================
+        List<ProductoMasVendidoDTO> ObtenerProductosMasVendidos(
+            DateTime fechaInicio,
+            DateTime fechaFin);
+
+        List<VentaPorDiaDTO> ObtenerVentasPorDia(
+            DateTime fechaInicio,
+            DateTime fechaFin);
+
+        // =========================
+        // CAJA
+        // =========================
+
+        decimal ObtenerBalanceCaja(
+            DateTime fechaInicio,
+            DateTime fechaFin);
+
+        List<CajaPorDiaDTO> ObtenerCajaPorDia(
+            DateTime fechaInicio,
+            DateTime fechaFin);
+
+        // =========================
+        // CRÉDITOS
+        // =========================
+
+        List<CreditoDTO> ObtenerCreditosVencidos();
+
+        List<CreditoDTO> ObtenerClientesConDeuda();
+
+        // =========================
+        // INVENTARIO
+        // =========================
+
+        List<ProductoInventarioDTO> ObtenerProductosBajoStock(
+            int stockMinimo);
+
+        // =========================
+        // DASHBOARD
+        // =========================
+
+        DashboardDTO ObtenerDashboard();
+    }
+}
---
+++ Sistema.Entities/DTOs/Ventas/VentaPorDiaDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sistema.Entities.DTOs.Ventas
+{
+    public class VentaPorDiaDTO
+    {
+        public DateTime Fecha { get; set; }
+        public decimal TotalVentas { get; set; }
+    }
+}
